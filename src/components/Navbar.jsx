8567663777d72a634a7bb51ebdf3f773865eddc5
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-10 18:04:52 +0530

    Binni | Add. Prettier to project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { styles } from "../styles";
+// import { navLinks } from "../constants";
+// import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  useEffect(() => {}, []);
 
-export default Navbar
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center `}>Nav</nav>
+  );
+};
+
+export default Navbar;
