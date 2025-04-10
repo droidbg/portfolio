1276bf9495e88d35b307a1afc8dfa0145a662fa1
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-11 00:37:01 +0530

    Binni | Add. responsiveness on 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -10,7 +11,7 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         {/* Circle + Line */}
-        <div className="flex flex-col justify-center items-center   mt-5">
+        <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-purple-custom" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
@@ -27,6 +28,23 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="h-[64px] w-[35px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 bg-secondary rounded-full"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
