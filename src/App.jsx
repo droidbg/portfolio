5b8757c1f468193a3995a11c5eb966160679e6cf
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-10 11:29:16 +0530

    Binni | Add. content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
   return (
     <>
-      Hi! I am Binni
-      <h1 class="text-3xl font-bold text-red-400">Hello world!</h1>
+      <h1 class="text-3xl font-bold text-red-400"> Hi! I am Binni!</h1>
     </>
   );
 }
