e4259b0ee8048ff4ea79828218b3b401cd771e99
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-10 20:07:40 +0530

    Binni | Fix. styling issues

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
