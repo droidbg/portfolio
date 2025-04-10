9e56905eed6251c7d4b53a5bd2d3ac3cf9270b91
Binni G <41873024+droidbg@users.noreply.github.com>
2025-04-10 20:26:22 +0530

    Binni | Fix. menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
             }}
           />
           <div
-            className={`${toggle ? "flex" : "hidden"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            className={`${toggle ? "flex" : "hidden"} p-6 black-gradient absolute top-15 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className="list-none flex flex-col justify-end items-start gap-4">
               {navLinks.map((link) => {
